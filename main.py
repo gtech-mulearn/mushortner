4d3491fb0f3a0fccfc827d461f09edcd9d907245
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-06-30 22:41:39 +0530

    db converted to models

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-from flask import Flask, request
+from flask import Flask, redirect
 from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
 
-
+from models.connection import DBConnection
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/mu_dev'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 api = Api(app)
+db = DBConnection()
 
 
+class UrlShortnerAPI(Resource):
+    def get(self, short_url):
+        query = "SELECT long_url FROM `url_shortener` where short_url = :short_url; "
+        params = {'short_url': short_url}
+        url = db.fetch_single_data(query, params)
+        if url:
+            return redirect(url)
+        return {
+            "hasError": True,
+            "statusCode": 400,
+            "message": "Url Not Found",
+            "response": {}
+        }
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'message': 'Hello, World!'}
-
-    def post(self):
-        data = request.get_json()
-        name = data['name']
-        return {'message': f'Hello, {name}!'}
-
-
-api.add_resource(HelloWorld, '/')
+api.add_resource(UrlShortnerAPI, '/get/<string:short_url>/')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/connection.py
@@ -0,0 +1,41 @@
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from sqlalchemy.pool import QueuePool
+
+
+class DBConnection:
+    def __init__(self, pool_size=5, max_overflow=10):
+        db_host = config("db_host")
+        db_user = config("db_user")
+        db_pass = config("db_pass")
+        db_name = config("db_name")
+        self.db_engine = create_engine(
+            url=f'mysql+pymysql://{db_user}:{db_pass}@{db_host}:3306/{db_name}',
+            poolclass=QueuePool,
+            pool_size=pool_size,
+            max_overflow=max_overflow
+        )
+
+    def execute(self, query, params=None):
+        with Session(self.db_engine) as session:
+            session.execute(text(query), params)
+            session.commit()
+
+    def fetch_all(self, query, params=None):
+        with Session(self.db_engine) as session:
+            result = session.execute(text(query), params)
+            session.commit()
+            return result.fetchall()
+
+    def fetch_one(self, query, params=None):
+        with Session(self.db_engine) as session:
+            result = session.execute(text(query), params)
+            session.commit()
+            return result.fetchone()
+
+    def fetch_single_data(self, query, params=None):
+        result = self.fetch_one(query, params)
+        if result and result[0]:
+            return result[0]
